95353a05721f82e5a29be40884057e67b9a25988
Patrick Petty <InductedCloud76@xboxlivefamily.com>
2024-01-18 01:04:32 -0700

    readability

--- DiceRoller.cs
+++ DiceRoller.cs
@@ -13,27 +13,34 @@ namespace Mission2
         {
             //get the input for the number of rolls
             int rolls = numRolls;
+
             //create an array to hold a count of each possible sum of two dice
             int[] counts = new int[11];
+
             //create a new random object
             Random random = new Random();
+
             //loop to roll two six sided dice
             for (int i = 0; i < rolls; i++)
             {
                 // Roll two six-sided dice
                 int die1 = random.Next(1, 7);
                 int die2 = random.Next(1, 7);
+
                 //add the die together
                 int sum = die1 + die2;
+
                 //add the sum to the counts array
                 //you have to subtract 2 from the sum because arrays start at position 0 and 
                 //the lowest possible combination for two dice is 2, so the positions are offset by 2
                 //ie: '2' is at position 0, '3' is at position 1, etc.
                 counts[sum - 2]++;
             }
+
             //initialize variables needed for later
             string output = "";
             string[] temp = new string[11];
+
             //loop to calculate percentages and add '*' to the output
             for (int i = 0; i < counts.Length; i++)
             {
@@ -45,6 +52,7 @@ namespace Mission2
                 }
                 output += (i + 2) + ": " + temp[i] + "\n\n";
             }
+
             //write the output to the console
             Console.Write(output);
         }
--- Program.cs
+++ Program.cs
@@ -6,14 +6,18 @@ internal class Program
     {
         //create a new instance of DiceRoller
         DiceRoller dr = new DiceRoller();
+
         //initial message the user in the console sees
         Console.WriteLine("Welcome to the dice rolling simulator");
         Console.WriteLine("How many dice rolls would you like to simulate?");
         string input = Console.ReadLine();
+
         //get the number of rolls from the user
         int numRolls = int.Parse(input);
+
         //call the RollDice method and pass the number of rolls to it
         dr.RollDice(numRolls);
+
         //goodbye
         Console.Write("Thank you for using the dice throwing simulator. Goodbye!");
     }
