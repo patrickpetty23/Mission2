bd91f32b2b94362eb011fa4c5322b9a8d38a1984
Patrick Petty <InductedCloud76@xboxlivefamily.com>
2024-01-18 01:02:08 -0700

    commenting code and Syncing to main

--- Program.cs
+++ Program.cs
@@ -4,14 +4,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //create a new instance of DiceRoller
         DiceRoller dr = new DiceRoller();
-
+        //initial message the user in the console sees
         Console.WriteLine("Welcome to the dice rolling simulator");
         Console.WriteLine("How many dice rolls would you like to simulate?");
         string input = Console.ReadLine();
-
+        //get the number of rolls from the user
         int numRolls = int.Parse(input);
-
+        //call the RollDice method and pass the number of rolls to it
         dr.RollDice(numRolls);
+        //goodbye
+        Console.Write("Thank you for using the dice throwing simulator. Goodbye!");
     }
 }
